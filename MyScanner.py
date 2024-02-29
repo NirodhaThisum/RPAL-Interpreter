84ce299693557ca6a625b9624becb9008f2dcc52
Nirodha Thisum <thisumnirodha@gmail.com>
2024-02-29 16:55:57 +0530

    Add punctions  strings(still have to complete)

--- MyScanner.py
+++ MyScanner.py
@@ -26,7 +26,7 @@ operator_symbol = [
     "!",
     "#",
     "%",
-    "ˆ",
+    "^",
     "_",
     "[",
     "]",
@@ -68,4 +68,44 @@ with open("RPAL.txt", "r") as f:
         elif inputString[i] in operator_symbol:
             token = inputString[i]
             print(token)
+
+        elif inputString[i] == ' ' or inputString[i] == '\t' or inputString[i] == '\n':
+            temp = i
+            while i + 1 < len(inputString) and (inputString[i + 1] == ' ' or inputString[i + 1] == '\t' or inputString[i + 1] == '\n'):
+                i += 1
+            token = inputString[temp : i + 1]
+            print(token)
+        
+        elif inputString[i] == '(':
+            token = '('
+            print(token)
+
+        elif inputString[i] == ')':
+            token = ')'
+            print(token)
+
+        elif inputString[i] == ';':
+            token = ';'
+            print(token)
+        
+        elif inputString[i] == ',':
+            token = ','  
+            print(token)
+
+        
+        elif inputString[i] == '\"':
+            temp = i
+            while(i + 1 < len(inputString) and (
+                inputString[i + 1] == '\t'
+                or inputString[i + 1] == '\n'
+                or inputString[i + 1] == '\\'
+                or inputString[i + 1] == '\"'
+
+            )):
+                i += 1
+                 
+            token = inputString[i :i + 1]
+            print(token)
+
+            
         i += 1
