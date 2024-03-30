01d6121e0f760608a4726a8135f636d86b2b3337
Roshane Moraes <roshanem1772@gmail.com>
2024-03-30 18:05:40 +0530

    Create MyParser.py

---
+++ MyParser.py
@@ -0,0 +1,10 @@
+operators = ['+', '-', '*', '<', '>', '&', '.', '@', '/', ':', '=', '~', '|', '$', '!', '#', '%',
+             '^', '_', '[', ']', '{', '}', '"', '`', '?']
+
+binary_operators = ["+", "-", "*", "/", "**", "gr", "ge", "<",
+                    "<=", ">", ">=", "ls", "le", "eq", "ne", "&", "or", "><"]
+
+keys = ["let", "fn", "in", "where", "aug", "or", "not", "true", "false", "nil", "dummy", "within",
+        "and", "rec", "gr", "ge", "ls", "le", "eq", "ne"]
+
+print("Enter the expression: ")
