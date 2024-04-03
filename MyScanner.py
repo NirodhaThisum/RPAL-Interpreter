a6d1aa2bf241470a35bdebfe6c8a9657df1aa570
Nirodha Thisum <thisumnirodha@gmail.com>
2024-04-03 09:44:32 +0530

    Initilize parser

--- MyScanner.py
+++ MyScanner.py
@@ -47,134 +47,136 @@ comment_elements = ['"', "\\", " ", "\t"]
 
 Input_Tokens = []
 
-with open("RPAL.txt", "r") as f:
-    inputString = f.read()
-    # print(inputString)
+def Scanning():
+    with open("RPAL.txt", "r") as f:
+        inputString = f.read()
+        # print(inputString)
 
-    i = 0
-    while i < len(inputString):
+        i = 0
+        while i < len(inputString):
 
-        if inputString[i].isalpha():
-            temp = i
-            while i + 1 < len(inputString) and (
-                (inputString[i + 1].isalpha())
-                or (inputString[i + 1].isdigit())
-                or (inputString[i + 1] == "_")
-            ):
-                i += 1
-            token = inputString[temp : i + 1]
-            Input_Tokens.append(Token(token, "<IDENTIFIER>"))
+            if inputString[i].isalpha():
+                temp = i
+                while i + 1 < len(inputString) and (
+                    (inputString[i + 1].isalpha())
+                    or (inputString[i + 1].isdigit())
+                    or (inputString[i + 1] == "_")
+                ):
+                    i += 1
+                token = inputString[temp : i + 1]
+                Input_Tokens.append(Token(token, "<IDENTIFIER>"))
 
-        elif inputString[i].isdigit():
-            temp = i
-            while i + 1 < len(inputString) and inputString[i + 1].isdigit():
-                i += 1
-            token = inputString[temp : i + 1]
-            Input_Tokens.append(Token(token, "<INTEGER>"))
+            elif inputString[i].isdigit():
+                temp = i
+                while i + 1 < len(inputString) and inputString[i + 1].isdigit():
+                    i += 1
+                token = inputString[temp : i + 1]
+                Input_Tokens.append(Token(token, "<INTEGER>"))
 
-        elif inputString[i] == " " or inputString[i] == "\t" or inputString[i] == "\n":
-            temp = i
-            while i + 1 < len(inputString) and (
-                inputString[i + 1] == " "
-                or inputString[i + 1] == "\t"
-                or inputString[i + 1] == "\n"
-            ):
-                i += 1
-            token = inputString[temp : i + 1]
-            Input_Tokens.append(Token(repr(token), "<DELETE>"))
-
-        elif inputString[i] == "(":
-            token = "("
-            Input_Tokens.append(Token("(", "("))
-
-        elif inputString[i] == ")":
-            token = ")"
-            Input_Tokens.append(Token(")", ")"))
-
-        elif inputString[i] == ";":
-            token = ";"
-            Input_Tokens.append(Token(";", ";"))
-
-        elif inputString[i] == ",":
-            token = ","
-            Input_Tokens.append(Token(",", ","))
-
-        elif inputString[i] == '"':
-            temp = i
-            while (
-                i + 1 < len(inputString)
-                and (
-                    inputString[i + 1] == "\t"
+            elif inputString[i] == " " or inputString[i] == "\t" or inputString[i] == "\n":
+                temp = i
+                while i + 1 < len(inputString) and (
+                    inputString[i + 1] == " "
+                    or inputString[i + 1] == "\t"
                     or inputString[i + 1] == "\n"
-                    or inputString[i + 1] == "\\"
-                    or inputString[i + 1] == "("
-                    or inputString[i + 1] == ")"
-                    or inputString[i + 1] == ";"
-                    or inputString[i + 1] == ","
-                    or inputString[i + 1] == " "
+                ):
+                    i += 1
+                token = inputString[temp : i + 1]
+                Input_Tokens.append(Token(repr(token), "<DELETE>"))
+
+            elif inputString[i] == "(":
+                token = "("
+                Input_Tokens.append(Token("(", "("))
+
+            elif inputString[i] == ")":
+                token = ")"
+                Input_Tokens.append(Token(")", ")"))
+
+            elif inputString[i] == ";":
+                token = ";"
+                Input_Tokens.append(Token(";", ";"))
+
+            elif inputString[i] == ",":
+                token = ","
+                Input_Tokens.append(Token(",", ","))
+
+            elif inputString[i] == '"':
+                temp = i
+                while (
+                    i + 1 < len(inputString)
+                    and (
+                        inputString[i + 1] == "\t"
+                        or inputString[i + 1] == "\n"
+                        or inputString[i + 1] == "\\"
+                        or inputString[i + 1] == "("
+                        or inputString[i + 1] == ")"
+                        or inputString[i + 1] == ";"
+                        or inputString[i + 1] == ","
+                        or inputString[i + 1] == " "
+                        or inputString[i + 1].isalpha()
+                        or inputString[i + 1].isdigit()
+                        or inputString[i + 1] in operator_symbol
+                    )
+                    and inputString[i + 1] != '"'
+                ):
+                    i += 1
+                if i + 1 < len(inputString) and inputString[i + 1] == '"':
+                    i += 1
+                    token = inputString[temp : i + 1]
+                    Input_Tokens.append(Token(token, "<STRING>"))
+
+                else:
+                    raise MyCustomError('need " ')
+
+            elif (
+                inputString[i] == "/"
+                and (i + 1 < len(inputString))
+                and inputString[i + 1] == "/"
+            ):
+                temp = i
+                while i + 1 < len(inputString) and (
+                    (inputString[i + 1] in comment_elements)
+                    or inputString[i + 1] in punction
                     or inputString[i + 1].isalpha()
                     or inputString[i + 1].isdigit()
                     or inputString[i + 1] in operator_symbol
-                )
-                and inputString[i + 1] != '"'
-            ):
-                i += 1
-            if i + 1 < len(inputString) and inputString[i + 1] == '"':
-                i += 1
+                    and (not (inputString[i + 1] == "\n"))
+                ):
+                    i += 1
+
+
+                if i + 1 < len(inputString) and inputString[i + 1] == "\n":
+                    i += 1
+                    # token = inputString[temp : i + 1]   #with last newline
+                    token = inputString[temp:i]  # without newline
+                    Input_Tokens.append(Token(token, "<DELETE>"))
+
+                else:
+                    raise MyCustomError("need to end with newline")
+
+            elif inputString[i] in operator_symbol:
+                temp = i
+                while i + 1 < len(inputString) and inputString[i + 1] in operator_symbol:
+                    i += 1
                 token = inputString[temp : i + 1]
-                Input_Tokens.append(Token(token, "<STRING>"))
+                Input_Tokens.append(Token(token, "<OPERATOR>"))
 
-            else:
-                raise MyCustomError('need " ')
-
-        elif (
-            inputString[i] == "/"
-            and (i + 1 < len(inputString))
-            and inputString[i + 1] == "/"
-        ):
-            temp = i
-            while i + 1 < len(inputString) and (
-                (inputString[i + 1] in comment_elements)
-                or inputString[i + 1] in punction
-                or inputString[i + 1].isalpha()
-                or inputString[i + 1].isdigit()
-                or inputString[i + 1] in operator_symbol
-                and (not (inputString[i + 1] == "\n"))
-            ):
-                i += 1
+            i += 1
 
 
-            if i + 1 < len(inputString) and inputString[i + 1] == "\n":
-                i += 1
-                # token = inputString[temp : i + 1]   #with last newline
-                token = inputString[temp:i]  # without newline
-                Input_Tokens.append(Token(token, "<DELETE>"))
-
-            else:
-                raise MyCustomError("need to end with newline")
-
-        elif inputString[i] in operator_symbol:
-            temp = i
-            while i + 1 < len(inputString) and inputString[i + 1] in operator_symbol:
-                i += 1
-            token = inputString[temp : i + 1]
-            Input_Tokens.append(Token(token, "<OPERATOR>"))
-
-        i += 1
+    # for token in Input_Tokens:
+    #     print(token.value, token.type)
 
 
-# for token in Input_Tokens:
-#     print(token.value, token.type)
+    # Screening
+    Tokens = []
+
+    for token in Input_Tokens:
+        if token.type != "<DELETE>":
+            Tokens.append(token)
 
 
-# Screening
-Tokens = []
-
-for token in Input_Tokens:
-    if token.type != "<DELETE>":
-        Tokens.append(token)
-
-
-for token in Tokens:
-    print(token.value, token.type)
-
+    # for token in Tokens:
+    #     print(token.value, token.type)
+    
+    return Tokens
---
+++ Parser.py
@@ -0,0 +1,56 @@
+from MyScanner import Scanning
+from MyScanner import Token
+
+tokens = Scanning()
+
+
+class ASTNode:
+    def __init__(self, type, value):
+        self.firstChild = None
+        self.sibling = None
+        self.value = value
+        self.type = type  # This is the printing argument of AST node
+        self.indentation = 0
+
+
+class ASTParser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.current_token = None
+        self.index = 0  # This is to track the current reading position
+        self.stack = []
+
+    def read(self, value, type):
+        self.current_token = tokens[self.index]
+        if self.current_token.type in ["<IDENTIFIER>", "<INTEGER>", "<STRING>"]:
+
+            currentTokenType = (
+                "<" + self.current_token.type + ":" + self.current_token.value + ">"
+            )
+            currentTokenValue = self.current_token.value
+            terminalNode = ASTNode(currentTokenType, currentTokenValue)
+            self.stack.append(terminalNode)
+
+        # if self.current_token.value in ["true", "false", "nil", "dummy"]:
+        #     stack.append(ASTNode(self.current_token.value))
+
+        # print("reading : " + str(self.current_token.value))
+
+        # Pick the next token
+        self.index += 1
+        if self.index < len(self.tokens):
+            self.current_token = self.tokens[self.index]
+
+    def testPrint(self):
+        for token in self.stack:
+            print(token.value, token.type)
+
+
+myParser = ASTParser(tokens)
+# for token in myParser.tokens:
+#     print(token.value, token.type)
+
+numberOfTokens = len(tokens)
+for i in range(numberOfTokens):
+    myParser.read()
+myParser.testPrint()
